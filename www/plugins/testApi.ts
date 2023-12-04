0065f460de1427a21973c00de915b8fedd3d338b
gleb-bread <glebhets123@gmail.com>
2023-12-04 10:01:31 +0300

    init testing api

--- www/plugins/testApi.ts
+++ www/plugins/testApi.ts
@@ -2,25 +2,25 @@ import { v4 } from "uuid";
 import { useCookie } from "nuxt/app";
 
 export default defineNuxtPlugin((app) => {
-    // const testApi = async function(){
+    const testApi = async function(){
 
         
-    //     let tokenUser = useCookie('user_token');
+        let tokenUser = useCookie('user_token');
 
-    //     let params = {
-    //         'user_token': tokenUser,
-    //     };
+        let params = {
+            'user_token': tokenUser,
+        };
 
 
-    //     const {data, status} = await useLazyFetch('http://localhost:8888//api/rights/names', { 
-    //         method: 'GET',
-    //         query: params,
-    //     });
+        const {data, status} = await useLazyFetch('http://localhost:8888//api/parser_products', { 
+            method: 'GET',
+            query: params,
+        });
 
-    //     console.log(data.value);
+        console.log(data.value);
 
-    // }
+    }
 
-    // app.vueApp
-    //     .use(testApi);
+    app.vueApp
+        .use(testApi);
 })
