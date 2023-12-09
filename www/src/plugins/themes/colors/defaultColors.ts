a3649676ae200f1db0fe936a385429cea425c938
gleb-bread <glebhets123@gmail.com>
2023-12-09 13:24:19 +0300

    add final depences

--- www/src/plugins/themes/colors/defaultColors.ts
+++ www/src/plugins/themes/colors/defaultColors.ts
@@ -1,3 +1,12 @@
+import { twColors } from "./twColors"
+
 export const defaultColors = {
-    'primary': '#1b0064',
+    background: twColors.indigo[50],
+    surface: "#FFFFFF",
+    primary: twColors.orange[500],
+    secondary: twColors.orange[700],
+    error: twColors.red[700],
+    info: twColors.teal[700],
+    success: twColors.lime[700],
+    warning: twColors.yellow[900],
 }
