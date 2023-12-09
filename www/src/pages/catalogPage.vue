29b7ba55e9e613e9478f3c15897a7555ef695a45
gleb-bread <glebhets123@gmail.com>
2023-12-09 18:19:00 +0300

    com

--- www/src/pages/catalogPage.vue
+++ www/src/pages/catalogPage.vue
@@ -1,5 +1,30 @@
 <template>
-    
+    <template v-if="parserProductsStore.getLoading || parserProductsStore.getError">
+    <div
+      class="h-screen w-screen d-flex justify-center"
+      :class="{ 'align-center': !parserProductsStore.getError }">
+      <v-progress-circular
+          v-if="parserProductsStore.getLoading && !parserProductsStore.getError"
+          indeterminate
+          :size="58"
+          :width="8"
+          color="primary">
+        </v-progress-circular>
+        <v-alert
+          v-if="parserProductsStore.getError"
+          max-height="100px"
+          type="error"
+          rounded="0"
+          title="Ошибка"
+          text="Произошла ошибка загрузки">
+        </v-alert>
+      </div>
+  </template>
+  <template
+    v-else>
+    <index-components-categories>
+    </index-components-categories>
+  </template>
 </template>
 
 <script lang='ts'>
