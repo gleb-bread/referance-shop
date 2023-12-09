f20b84c0af0cbf7da7c5bb84a6eb4c96f92150bc
gleb-bread <glebhets123@gmail.com>
2023-12-09 18:20:03 +0300

    com

--- www/src/widgets/catalogPage/gridCategories.vue
+++ www/src/widgets/catalogPage/gridCategories.vue
@@ -1,5 +1,21 @@
 <template>
-    
+    <v-virtual-scroll
+        class="pa-10"
+        :items="StructHelper.chunk<string>(Object.keys(getImageCategories), currectCell.cells)">
+        <template #default="{ item }">
+            <v-row>
+                <v-col
+                    :cols="currectCell.size"
+                    v-for="categoryKey in item">
+                    <category-item
+                        @click.stop="$nuxt.$router.replace({path: '/category'})"
+                        :category-img="getImageCategories[categoryKey]"
+                        :category-title="categoryKey">
+                    </category-item>
+                </v-col>
+            </v-row>
+        </template>
+    </v-virtual-scroll>
 </template>
 
 <script lang='ts'>
