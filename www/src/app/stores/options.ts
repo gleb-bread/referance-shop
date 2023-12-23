1bf37bd2988f257cd0edc7b3b9f0067f2e5fccd0
gleb-bread <glebhets123@gmail.com>
2023-12-23 09:43:44 +0300

    add package

--- www/src/app/stores/options.ts
+++ www/src/app/stores/options.ts
@@ -1,5 +1,5 @@
 import { getCookie } from "@/shared/helpers/helperCookie";
-export const publicPath = 'http://localhost:8888/';
+export const publicPath = 'https://copyrain.ru/';
 
 export const getCurrectData = function(params: any){
     return {
